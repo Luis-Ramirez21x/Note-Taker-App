4495da9fb075133342aa3236372f6f0990332f03
Luis Ramirez <luis21x@outlook.com>
2021-11-10 19:08:54 -0500

    simplifying post call, coming up with invalid arg type

--- Develop/server.js
+++ Develop/server.js
@@ -17,55 +17,28 @@ app.use(express.static('public'));
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
-    console.log('home');
 });
 
 app.get('/notes', (req,res) =>{
     res.sendFile(path.join(__dirname, '/public/notes.html'));
-    console.log('notes');
 });
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
+
 
 app.get('/api/notes', (req,res) => {
-    res.json(notes);
-    console.log('getting saved notes');
+    res.json(currentDataBase);
 });
 
-app.post('/api/notes', (req,res) =>{
-    console.log(`${req.method} method received`);
-
-    const { title, text}  = req.body;
-    
-    if(title && text){
-    const note = {
-        title,
-        text,
-        id: uid(),
-    };
-
-    fs.readFile('./db/db.json', 'utf8', (err,data) =>{
-        if(err){
-            console.log(err);
-        }else {
-            const parsedNotes = JSON.parse(data);
-
-            parsedNotes.push(note);
-
-            fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (writeErr) =>
-            (writeErr) =>
-            writeErr
-              ? console.error(writeErr)
-              : console.info('Successfully updated reviews!')
-            );
-        }
-    });
-
-
-
-        res.json(note);
-    }else {
-        res.status(500).json('Error in posting Note');
-    }
-})
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    newNote.id = uid();
+    currentDataBase.push(newNote);
+    fs.writeFileSync(notes, JSON.stringify(currentDataBase));
+    res.json(newNote);
+  });
 
 
 app.listen(PORT, () => 
