bac62f79b6d23d83fd1418afce75faf55cb0a9f0
Luis Ramirez <luis21x@outlook.com>
2021-11-10 21:40:22 -0500

    fixing sync with get api call and db

--- Develop/server.js
+++ Develop/server.js
@@ -1,36 +1,32 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const uid = require('./helper/uniqueId');
-
-const PORT = 3001;
-const notes = require('./db/db.json');
-const dataBase = fs.readFileSync("./db/db.json");
-const currentDataBase = JSON.parse(dataBase);
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const uid = require("./helper/uniqueId");
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+const notes = "./db/db.json";
+let dataBase = fs.readFileSync(notes);
+let currentDataBase = JSON.parse(dataBase);
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.static('public'));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+app.get("/api/notes", (req, res) => res.json(currentDataBase));
 
-app.get('/notes', (req,res) =>{
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
 
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
 );
-
-
-app.get('/api/notes', (req,res) => {
-    res.json(currentDataBase);
-});
 
 app.post("/api/notes", (req, res) => {
     const newNote = req.body;
